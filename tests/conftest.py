09c9bb86f7e0cd8721b1104618ea36834bf78cef
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2025-08-14 10:48:38 -0400

    Cleaner way to override a config value

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,9 @@ from pytest import fixture
 
 @fixture(scope="session", autouse=True)
 def set_config():
-    with (
-        gifnoc.use(Path(__file__).parent / "test-config.yaml"),
-        TemporaryDirectory() as tmpdir,
-    ):
-        from paperoni.config import config
-
-        config.data_path = Path(tmpdir) / "data"
-        yield
+    with TemporaryDirectory() as tmpdir:
+        with gifnoc.use(
+            Path(__file__).parent / "test-config.yaml",
+            {"paperoni.data_path": str(Path(tmpdir) / "data")},
+        ):
+            yield
